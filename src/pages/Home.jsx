f7399d46c72a00bb4fc1123319659d23848052e0
Yale Araujo <yale850@gmail.com>
2023-01-30 12:01:01 -0300

    adding text input and button opacity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,54 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native';
 
 export function Home() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>React Native</Text>
-            <Text>Ignite</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome, Yale Araújo!</Text>
+            <TextInput style={styles.input} placeholderTextColor='#555' placeholder='Type your skill'/>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>
+                    Add skill
+                </Text>
+            </TouchableOpacity>
+
+            <Text style={[styles.title, {marginTop: 50}]}>My skills</Text>
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingHorizontal: 20,
+        paddingVertical: 70,
+        paddingHorizontal: 30,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1f1e25',
+        color: '#fff',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7,
+    },
+    button: {
+        backgroundColor: '#a370f7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 17
+    }
+})
